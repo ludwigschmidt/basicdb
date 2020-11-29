3fcb4b963ccb03e403adb2816c96e1310d8e11ba
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2020-11-28 22:59:38 -0800

    Version 0.0.12

--- tests/test_basicdb.py
+++ tests/test_basicdb.py
@@ -11,7 +11,7 @@ from basicdb import __version__, BasicDB, DeletionError, IntegrityError, Namespa
 
 
 def test_version():
-    assert __version__ == '0.0.11'
+    assert __version__ == '0.0.12'
 
 def simple_test(db):
     db.insert(name='test1',
